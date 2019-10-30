7458233293f9a0d6df655c735f12a01ea355bf76
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-10-30 12:01:39 +0800

    1
    1

--- jisuanqi/ViewController.swift
+++ jisuanqi/ViewController.swift
@@ -9,7 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var number1: UITextField!
+    
+    @IBOutlet weak var number2: UITextField!
+    
+    @IBOutlet weak var coperator: UITextField!
+    
+    @IBOutlet weak var result: UITextField!
+    @IBAction func add(_ sender: UIButton) {
+        coperator.text = "+"
+    }
+    @IBAction func minus(_ sender: UIButton) {
+        coperator.text = "-"
+    }
+    @IBAction func chen(_ sender: UIButton) {
+        coperator.text = "*"
+    }
+    
+    @IBAction func chu(_ sender: UIButton) {
+        coperator.text = "/"
+    }
+    
+    @IBAction func calculate(_ sender: UIButton) {
+        var Num1 = Double(number1.text!)!
+        var Num2 = Double(number2.text!)!
+        if(coperator.text == "+")
+        {
+            var sum = Num1 + Num2
+            result.text = String(sum)
+        }
+        if(coperator.text == "-")
+        {
+            var sum = Num1 - Num2
+            result.text = String(sum)
+        }
+        if(coperator.text == "*")
+        {
+            var sum = Num1 * Num2
+            result.text = String(sum)
+        }
+        if(coperator.text == "/")
+        {
+            var sum = Num1 / Num2
+            result.text = String(sum)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
